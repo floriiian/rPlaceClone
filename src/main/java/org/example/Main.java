c70ce841aab64d84ca12ed1852418054ea8d4905
Florian S. <112857696+floriiian@users.noreply.github.com>
2024-09-27 10:07:34 +0200

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) {
+
+        LOGGER.debug("Ran");
+
+    }
+}
+
+
